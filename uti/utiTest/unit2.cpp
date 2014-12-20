3f06e2e1a18a3cb3bb41b033cc89e7aba1fb6b63
Marvin Pohl <mp078@hdm-stuttgart.de>
2014-12-20 10:42:44 +0000

    Added UTF16String to second unit test

--- uti/utiTest/unit2.cpp
+++ uti/utiTest/unit2.cpp
@@ -6,6 +6,8 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 template __declspec( dllexport ) class uti::UTF8String<>;
 
+template __declspec( dllexport ) class uti::UTF16String<>;
+
 typedef uti::UTF8String<> String;
 
 namespace utiTest
